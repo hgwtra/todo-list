7736d7a0d103e2c5fda12aa841f1f4a4fa1526cc
Trà <huong.mai198@gmail.com>
2023-11-26 01:47:52 +0200

    wip

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment =  {
+    production: true,
+    
+    //Firebase Configuration
+    firebaseConfig: {
+        apiKey: "AIzaSyAeszUNuavlHPxYMO-XzuO7DswIl5_9AzA",
+        authDomain: "todo-list-b1dd4.firebaseapp.com",
+        projectId: "todo-list-b1dd4",
+        storageBucket: "todo-list-b1dd4.appspot.com",
+        messagingSenderId: "396660929736",
+        appId: "1:396660929736:web:2ff1d4ba61740ee1b521fd"
+    }
+    
+}
